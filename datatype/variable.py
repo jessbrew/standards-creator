81bc87a04f3456c80944619d3ac20ff78323983c
BenAleckson <126693278+BenAleckson@users.noreply.github.com>
2025-04-02 09:09:04 -0500

    added sqlalchemy to variable class

--- datatype/variable.py
+++ datatype/variable.py
@@ -1,4 +1,17 @@
-class Variable:
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+engine = create_engine("sqlite://")
+base = declarative_base()
+
+class Variable(base):
+    __tablename__ = "Variable"
+
+    variableID = Column(Integer, primary_key=True, autoincrement=True)
+    questionID = Column(Integer)
+    name = Column(String)
+    detials = Column(String)
+
     def __init__(self, variableID, questionID, name, details):
         self.variableID = variableID
         self.questionID = questionID
